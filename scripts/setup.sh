81d77f738dd5669f28eac735c67a0b5628804b37
mesk <mesk97@gmail.com>
2023-01-16 22:37:06 +0300

    new links

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,9 +2,13 @@
 set -x
 (cd ../ && git submodule update) 
 
-pwd
 
 sudo apt install lynx
 sudo apt install iputils-arping
 chmod +x scripts/*.sh
+
+cd scripts \
+	&& ln -sf arch_tools.sh zcat \
+	&& ln -sf arch_tools.sh zgrep \
+	&& ln -sf arch_tools.sh zmore
 
