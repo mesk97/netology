23bfcf2bd7ec3f739ea24bf1b5f85022337091cd
mesk <mesk97@gmail.com>
2023-02-01 18:17:01 +0300

    setup

--- init/setup.sh
+++ init/setup.sh
@@ -1,5 +1,18 @@
+#!/bin/bash
+
+git clone https://github.com/utshina/uefi-simple
+sudo apt install gcc-mingw-w64-x86-64-posix
+
+patch -N -p0 /etc/default/grub patch_grub
+#diff -Nur original_grub /etc/default/grub
+
 # remove grub loader
 
 # timeout 
 /etc/default/grub
+
+efibootmgr
+efibootmgr -t 0
+efibootmgr --delete-bootnum -b 0003
+
 
