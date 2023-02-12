37caac98a50f7b6f4b55a1e3f5f5cb6cc5b77e64
mesk <mesk97@gmail.com>
2023-02-12 23:16:14 +0300

    new dhcp

---
+++ dhcp/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get install isc-dhcp-server
+
+# set IP in ubuntu
+# sudo nmcli connection modify cf79158c-1e3f-35e5-83d8-6b4a26462748 ipv4.address 192.168.2.4/24
+
+# show info
+# nmcli connection show
