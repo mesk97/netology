9188b6e61e529f9055d4e9a3a5b977be3f547d9f
mesk <mesk97@gmail.com>
2023-02-18 16:48:22 +0300

    updated

--- dhcp/setup.sh
+++ dhcp/setup.sh
@@ -2,8 +2,14 @@
 
 sudo apt-get install isc-dhcp-server
 
-# set IP in ubuntu
-# sudo nmcli connection modify cf79158c-1e3f-35e5-83d8-6b4a26462748 ipv4.address 192.168.2.4/24
+# set static IP in ubuntu
+# sudo nmcli con mod cf79158c-1e3f-35e5-83d8-6b4a26462748 ipv4.address 10.1.1.2/24
+# sudo nmcli con mod cf79158c-1e3f-35e5-83d8-6b4a26462748 ipv4.method manual
+# restart VM to apply -> how to change ?
 
 # show info
 # nmcli connection show
+
+# setup TFTPD ubuntu 
+sudo apt-get install tftpd-hpa
+
